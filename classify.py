095939bd125cca7e48e217587880d4549d0624cb
SalomonKisters <141818828+SalomonKisters@users.noreply.github.com>
2024-06-23 01:38:04 +0200

    dd

--- classify.py
+++ classify.py
@@ -3,12 +3,13 @@ from transformers import AutoProcessor, AutoModelForCausalLM
 import os 
 import csv
 from tqdm import tqdm
-import random
+import torch
 
 directory = "content"
 results_path = "results"
 image_endings = [".jpg", ".jpeg", ".png", ".gif", ".bmp", ".tiff", ".webp"]
 file_number = len(os.listdir(results_path)) + 1
+device = "cuda:0" if torch.cuda.is_available() else "cpu"
 
 def find_images(base_folder):
     images = []
@@ -23,7 +24,7 @@ images = find_images(directory)
 print(f"Found {len(images)} images.")
 
 print("Initializing model")
-model = AutoModelForCausalLM.from_pretrained("microsoft/Florence-2-large-ft", trust_remote_code=True)
+model = AutoModelForCausalLM.from_pretrained("microsoft/Florence-2-large-ft", trust_remote_code=True).to(device)
 processor = AutoProcessor.from_pretrained("microsoft/Florence-2-large-ft", trust_remote_code=True)
 
 print("Processing images")
@@ -35,9 +36,9 @@ with open(tsv_path, 'w', newline='', encoding='utf-8') as tsvfile:
 
     for image_path in tqdm(images, desc="Processing Images", unit="image"):
         try:
-            image = Image.open(image_path)
+            image = Image.open(image_path).convert("RGB")
 
-            inputs = processor(text="<OD>", images=image, return_tensors="pt")
+            inputs = processor(text="<OD>", images=image, return_tensors="pt").to(device)
 
             generated_ids = model.generate(
                 input_ids=inputs["input_ids"],
@@ -48,10 +49,11 @@ with open(tsv_path, 'w', newline='', encoding='utf-8') as tsvfile:
             )
             generated_text = processor.batch_decode(generated_ids, skip_special_tokens=False)[0]
 
-            parsed_answer = processor.post_process_generation(generated_text, task="<OD>", image_size=(image.width, image.height))
+            parsed_answer = processor.post_process_generation(generated_text, task="<OD>", image_size=(image.width, image.height))['<OD>']['labels']
 
             writer.writerow([image_path, parsed_answer])
         except Exception as e:
             print(f"Error processing {image_path}: {str(e)}")
+
 
 print(f"Processing complete. Results saved in 'image_analysis_results_{file_number}.tsv'")
